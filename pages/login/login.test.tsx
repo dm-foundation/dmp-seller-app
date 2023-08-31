c62af4a473243a226daddf9bcedf213b533273c8
Gabriel Ferreira <gabrielcsf@gmail.com>
2023-08-31 12:59:27 -0300

    Removing test example until I fix the test infrastructure

--- pages/login/login.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Login } from '.';
-
-describe('Login page', () => {
-  it('has sign in description', () => {
-    render(<Login />);
-    expect(screen.getByText(/Ethereum address/)).toBeTruthy();
-  });
-});
