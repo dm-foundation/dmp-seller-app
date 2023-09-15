ee110beadbb2efdd22b99f414ca9ff5af6c099c2
Martin Becze <martin@becze.org>
2023-09-15 22:24:31 +0200

    added contracts submodule

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from '@wagmi/cli';
 import { foundry, react } from '@wagmi/cli/plugins';
-import * as chains from 'wagmi/chains';
-
-const address: string = 'b2F3475CC6bB75b4116dbfE586B1e1765CBbbcAB';
 
 export default defineConfig({
   out: 'src/generated.ts',
