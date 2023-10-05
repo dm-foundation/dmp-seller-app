8a2c332284433edad58d9fb9196e2c52742b8bd5
Felipe Almeida <felipealmeidadevelop@gmail.com>
2023-10-05 10:55:14 -0300

    implements update order status and items units when transaction is completed

--- pages/newsale/payment-confirmation/[...slug].tsx
+++ pages/newsale/payment-confirmation/[...slug].tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { buildPaymentConfirmationURL } from '@/lib/contract';
 import classes from '@/pages/App.module.css';
@@ -10,19 +10,47 @@ import { useEffect, useState } from 'react';
 import Layout from '../../../components/layout';
 import { patch } from '@/api/api';
 
-export default function Page() {
+type orderResponse = {
+  amountInEth: number;
+  amountInUSD: number;
+  amountInUSDC: number;
+  amountInWei: string;
+  created_at: string;
+  customer_email: string | null;
+  id: number;
+  orderItems: orderItem[];
+  paymentAddress: string;
+  paymentCurrency: string;
+  paymentFactoryAddress: string;
+  paymentProof: string;
+  paymentReceipt: string;
+  paymentTransactionHash: string | null;
+  status: string;
+  storeId: number;
+  updated_at: string;
+};
 
+
+type orderItem = {
+  id: number;
+  itemId: number;
+  orderId: number;
+  quantity: number;
+  storeId: number;
+  unitPrice: number;
+};
+
+export default function Page() {
   const [transactionConfirmation, setTransactionConfirmation] = useState<any>({});
-  const [transactionStatus, setTansactionStatus] = useState("PENDING");
+  const [transactionStatus, setTansactionStatus] = useState('PENDING');
   const router = useRouter();
   let checkPaymentInterval: any;
 
   useEffect(() => {
     const fetchTransactionConfirmation = async () => {
       const paymentConfirmationURL = buildPaymentConfirmationURL(router.query.slug as string);
-      if (!paymentConfirmationURL) { return; }
-
-      console.log("paymentConfirmationURL: ", paymentConfirmationURL);
+      console.log('router.query.slug: ', router.query.slug);
+      console.log('paymentConfirmationURL: ', paymentConfirmationURL);
       const paymentTransactionData = await axios.get(paymentConfirmationURL, {
         withCredentials: false,
         headers: {
@@ -32,67 +60,67 @@ export default function Page() {
         },
       });
 
-      console.log("paymentTransactionData: ", paymentTransactionData);
+      console.log('paymentTransactionData: ', paymentTransactionData);
       setTransactionConfirmation(paymentTransactionData.data);
-      setTansactionStatus("COMPLETE");
+      setTansactionStatus('COMPLETE');
 
-      if (transactionStatus === "COMPLETE") {
-        // patch(`/order/${24}/status`, { status: "complete" });
+      if (transactionStatus === 'COMPLETE') {
+        const orderResponseString = localStorage.getItem('orderResponse');
+        const orderResponse: orderResponse = JSON.parse(orderResponseString || '{}');
+
+        patch(`/order/${orderResponse.id}/status`, { status: 'complete' }).then(() => {
+          orderResponse.orderItems.map((orderItem: orderItem) => {
+            patch(`/item/${orderItem.id}/subtract-unit-item`, orderItem.quantity);
+          });
+        }).catch((err) => {
+          console.log(err);
+        });
+
+
       }
       clearInterval(checkPaymentInterval);
-    }
+    };
 
-    if (transactionStatus !== "COMPLETE") {
+    if (transactionStatus !== 'COMPLETE') {
       checkPaymentInterval = setInterval(() => {
         fetchTransactionConfirmation();
-      }, 7000)
+      }, 7000);
     }
-
-  }, [router.query.slug])
+  }, [router.query.slug]);
 
   return (
     <>
-      <Layout title="Transaction" >
-        <Flex
-          direction="column"
-          justify="center"
-          align="center"
-        >
-          {
-            transactionConfirmation?.status !== '1' &&
+      <Layout title="Transaction">
+        <Flex direction="column" justify="center" align="center">
+          {transactionConfirmation?.status !== '1' && (
             <>
               <Loader color="black" />
-              <h2>
-                Pending transaction..
-              </h2>
-              <Text>
-                Transaction will likely process in less than 30 seconds.
-              </Text>
+              <h2>Pending transaction..</h2>
+              <Text>Transaction will likely process in less than 30 seconds.</Text>
             </>
-          }
+          )}
 
-          {
-            transactionConfirmation?.status === '1' &&
+          {transactionConfirmation?.status === '1' && (
             <>
               <Image
                 radius="md"
                 h={200}
                 w="auto"
                 fit="contain"
-                src="../../../green-checkmark-icon.png" />
-              <h2>
-                Transaction complete
-              </h2>
-              <Text mb={20} >
-                Your transaction has been confirmed.
-              </Text>
-              <Link className={classes.link_transaction_confirmation} href={`https://etherscan.io/tx/${transactionConfirmation.result[0].hash}`} >
+                src="../../green-checkmark-icon.png"
+              />
+              <h2>Transaction complete</h2>
+              <Text mb={20}>Your transaction has been confirmed.</Text>
+              <Link
+                className={classes.link_transaction_confirmation}
+                href={`https://etherscan.io/tx/${transactionConfirmation.result[0].hash}`}
+              >
                 View on Block Explorer
               </Link>
             </>
-          }
+          )}
         </Flex>
-      </Layout >
+      </Layout>
     </>
   );
 }
--- pages/newsale/payment-scan/index.tsx
+++ pages/newsale/payment-scan/index.tsx
@@ -1,7 +1,14 @@
-"use client";
+'use client';
 
 import { AppContext } from '@/context';
-import { PaymentFactoryDefaultCurrency, PaymentFactoryDefaultProofAddress, PaymentFactoryReadContractGetPaymentAddress, PaymentFactoryMainnetContractAddress, buildPaymentContractParams, buildPaymentURI } from '@/lib/contract';
+import {
+  PaymentFactoryDefaultCurrency,
+  PaymentFactoryDefaultProofAddress,
+  PaymentFactoryReadContractGetPaymentAddress,
+  PaymentFactoryMainnetContractAddress,
+  buildPaymentContractParams,
+  buildPaymentURI,
+} from '@/lib/contract';
 import CryptoConverter from '@/lib/currency';
 import { sha256Hasher } from '@/lib/hashing';
 import classes from '@/pages/App.module.css';
@@ -10,75 +17,89 @@ import { encode } from '@ipld/dag-cbor';
 import { Button, Container, Flex, Paper, Table, Text } from '@mantine/core';
 import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
-import QRCode from "react-qr-code";
-import { useContractRead } from "wagmi";
+import QRCode from 'react-qr-code';
+import { useContractRead } from 'wagmi';
 import Layout from '../../../components/layout';
-import paymentFactoryABI from "../../../fixtures/PaymentFactory.json" assert { type: "json" };
+import paymentFactoryABI from '../../../fixtures/PaymentFactory.json' assert { type: 'json' };
 import { post } from '@/api/api';
 
 export default function PaymentScan() {
   const { walletStoreContext } = useContext(AppContext);
 
   const paymentCurrency = walletStoreContext?.paymentCurrency || PaymentFactoryDefaultCurrency;
-  const cartItems = walletStoreContext?.cart.filter(item => item.amount > 0) || [];
-  const amountInUSD: number = cartItems?.reduce((acc, item: Item) => acc + (item.price * Number(item.amount)), 0) || 0;
+  const cartItems = walletStoreContext?.cart.filter((item) => item.amount > 0) || [];
+  const amountInUSD: number =
+    cartItems?.reduce((acc, item: Item) => acc + item.price * Number(item.amount), 0) || 0;
   const amountInUSDC: number = amountInUSD * 1000000 || 0;
 
-  const merchantAddress = walletStoreContext?.ethAddress || "0x0000000000000000000000000000000000000000";
+  const merchantAddress =
+    walletStoreContext?.ethAddress || '0x0000000000000000000000000000000000000000';
 
   const [isOrderPersisted, setIsOrderPersisted] = useState(false);
   const [amountInEth, setAmountInEth] = useState<number>(0);
   const [amountInWei, setAmountInWei] = useState<number>(0);
   const [contractParams, setContractParams] = useState<string[]>([]);
 
-  const [hashedCart, setHashedCart] = useState("");
-  let qrCodeURL: string = "";
+  const [hashedCart, setHashedCart] = useState('');
+  let qrCodeURL: string = '';
 
   const convertUSDtoPaymentCurrency = async (amountInUSD: number) => {
     setAmountInEth(await CryptoConverter.convertUSDtoETH(amountInUSD));
     setAmountInWei(await CryptoConverter.convertETHtoWei(amountInEth));
-  }
+  };
 
   const isValidQRCode = (uri: string) => {
     return uri && uri.length > 0 && !uri.includes('undefined');
-  }
+  };
 
   useEffect(() => {
-    console.log("[INFO] hashedCart: ", hashedCart);
+    console.log('[INFO] hashedCart: ', hashedCart);
     const fetchPaymentParamsData = async () => {
       if (walletStoreContext?.cart) {
         const saleJSON = encode(JSON.stringify(walletStoreContext?.cart));
         let hashedSaleJSON = sha256Hasher(saleJSON);
         setHashedCart(hashedSaleJSON);
       }
-      if (paymentCurrency === "USDC") {
-        setContractParams(buildPaymentContractParams(merchantAddress, paymentCurrency, amountInUSDC.toString(), `0x${hashedCart}`))
+      if (paymentCurrency === 'USDC') {
+        setContractParams(
+          buildPaymentContractParams(
+            merchantAddress,
+            paymentCurrency,
+            amountInUSDC.toString(),
+            `0x${hashedCart}`
+          )
+        );
       } else {
         await convertUSDtoPaymentCurrency(amountInUSD);
-        setContractParams(buildPaymentContractParams(merchantAddress, paymentCurrency, amountInWei.toString(), `0x${hashedCart}`))
+        setContractParams(
+          buildPaymentContractParams(
+            merchantAddress,
+            paymentCurrency,
+            amountInWei.toString(),
+            `0x${hashedCart}`
+          )
+        );
       }
-      console.log("[INFO] paymentCurrency: ", paymentCurrency)
-      console.log("[INFO] contractParams: ", contractParams);
-    }
+      console.log('[INFO] paymentCurrency: ', paymentCurrency);
+      console.log('[INFO] contractParams: ', contractParams);
+    };
 
     fetchPaymentParamsData();
-
   }, [hashedCart, amountInUSDC, amountInWei, paymentCurrency, qrCodeURL]);
-  qrCodeURL = "";
+  qrCodeURL = '';
 
   const contractData = useContractRead({
     address: PaymentFactoryMainnetContractAddress,
     abi: paymentFactoryABI.abi,
     functionName: PaymentFactoryReadContractGetPaymentAddress,
-    args: contractParams
+    args: contractParams,
   });
 
-  console.log("buildPaymentURI: ", qrCodeURL);
+  console.log('buildPaymentURI: ', qrCodeURL);
   if (contractData.data && !isOrderPersisted) {
-
     try {
-      console.log("contractParams: ", contractParams);
-      console.log("[INFO] Payment address", contractData.data);
+      console.log('contractParams: ', contractParams);
+      console.log('[INFO] Payment address', contractData.data);
 
       const orderData = {
         storeId: walletStoreContext?.storeId,
@@ -91,89 +112,116 @@ export default function PaymentScan() {
         paymentCurrency: paymentCurrency,
         paymentReceipt: hashedCart,
         paymentProof: PaymentFactoryDefaultProofAddress,
-        items: cartItems.map(item => ({
-          'itemId': item.id,
-          'quantity': item.amount,
-          'unitPrice': item.price
-        }))
-      }
+        items: cartItems.map((item) => ({
+          itemId: item.id,
+          quantity: item.amount,
+          unitPrice: item.price,
+        })),
+      };
       console.log(orderData);
-      post('/order', orderData);
-      setIsOrderPersisted(true);
+      post('/order', orderData)
+        .then((order) => {
+          if (order) {
+            localStorage.setItem('orderResponse', JSON.stringify(order));
+            setIsOrderPersisted(true);
+          } else {
+            console.log('[ERROR] Could not save transaction!');
+          }
+        })
+        .catch((error) => {
+          console.error('[ERROR] Could not save transaction!', error);
+        });
     } catch (e) {
-      console.log("[ERROR] Could not save transaction!", e);
+      console.log('[ERROR] Could not save transaction!', e);
     }
   }
 
   if (paymentCurrency === PaymentFactoryDefaultCurrency) {
-    qrCodeURL = buildPaymentURI(contractData.data as string, paymentCurrency, amountInUSDC.toString());
+    qrCodeURL = buildPaymentURI(
+      contractData.data as string,
+      paymentCurrency,
+      amountInUSDC.toString()
+    );
   } else {
-    qrCodeURL = buildPaymentURI(contractData.data as string, paymentCurrency, amountInWei.toString());
+    qrCodeURL = buildPaymentURI(
+      contractData.data as string,
+      paymentCurrency,
+      amountInWei.toString()
+    );
   }
 
   return (
     <>
       <Layout title="Scan to checkout">
-        <Flex
-          direction="column"
-          justify="center"
-          align="center"
-          gap={20}
-          mb={100}
-        >
-          {cartItems.length === 0 && <p className={classes.error}>Cart is empty! Start a new sale.</p>}
-          {!isValidQRCode(qrCodeURL) && walletStoreContext?.cart && <p className={classes.error}>Generating QR code for payment..</p>}
-          {isValidQRCode(qrCodeURL) &&
+        <Flex direction="column" justify="center" align="center" gap={20} mb={100}>
+          {cartItems.length === 0 && (
+            <p className={classes.error}>Cart is empty! Start a new sale.</p>
+          )}
+          {!isValidQRCode(qrCodeURL) && walletStoreContext?.cart && (
+            <p className={classes.error}>Generating QR code for payment..</p>
+          )}
+          {isValidQRCode(qrCodeURL) && (
             <>
               <Text>
                 To begin checkout, open the camera on your mobile device and scan the QR code below.
               </Text>
-              <Flex
-                direction="column"
-                justify="center"
-                align="center"
-                gap={10}
-                mb={20}
-              >
-                <Container mt={30} style={{ height: "auto", margin: "0 auto", maxWidth: 300, width: "100%" }}>
+              <Flex direction="column" justify="center" align="center" gap={10} mb={20}>
+                <Container
+                  mt={30}
+                  style={{ height: 'auto', margin: '0 auto', maxWidth: 300, width: '100%' }}
+                >
                   <QRCode
                     size={250}
-                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
                     value={qrCodeURL}
                   />
                 </Container>
-              </Flex >
+              </Flex>
               <Paper shadow="xs" p="xl">
                 <Text fz={'sm'}>
                   <b>Payment address:</b> {contractData.data as string}
                 </Text>
-                <Text fz={'xs'}><b>Contract parameters:</b></Text>
+                <Text fz={'xs'}>
+                  <b>Contract parameters:</b>
+                </Text>
                 <>
-                  {
-                    contractParams.map((p) => {
-                      return <Text fz={'xs'}>{p}</Text>
-                    })
-                  }
+                  {contractParams.map((p) => {
+                    return <Text fz={'xs'}>{p}</Text>;
+                  })}
                 </>
                 <Table mt={30} mb={30} fz={12}>
                   <Table.Thead>
                     <Table.Tr>
-                      <Table.Th><b>Total Payment Amount:</b></Table.Th>
+                      <Table.Th>
+                        <b>Total Payment Amount:</b>
+                      </Table.Th>
                       <Table.Th></Table.Th>
                     </Table.Tr>
                   </Table.Thead>
                   <Table.Tbody>
                     <Table.Tr>
                       <Table.Td></Table.Td>
-                      <Table.Td>{amountInUSD.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} USD</Table.Td>
+                      <Table.Td>
+                        {amountInUSD.toLocaleString('en-US', {
+                          style: 'currency',
+                          currency: 'USD',
+                        })}{' '}
+                        USD
+                      </Table.Td>
                     </Table.Tr>
-                    {paymentCurrency === "USDC" &&
+                    {paymentCurrency === 'USDC' && (
                       <Table.Tr>
                         <Table.Td></Table.Td>
-                        <Table.Td>{amountInUSD.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} USDC</Table.Td>
+                        <Table.Td>
+                          {amountInUSD.toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          })}{' '}
+                          USDC
+                        </Table.Td>
                       </Table.Tr>
-                    }
-                    {paymentCurrency !== "USDC" &&
+                    )}
+                    {paymentCurrency !== 'USDC' && (
                       <>
                         <Table.Tr>
                           <Table.Td></Table.Td>
@@ -184,11 +232,12 @@ export default function PaymentScan() {
                           <Table.Td>{amountInWei} WEI</Table.Td>
                         </Table.Tr>
                       </>
-                    }
+                    )}
                   </Table.Tbody>
                 </Table>
-                <Button color="dark" w={"100%"} size="lg">
-                  <Link style={{ textDecoration: 'none', color: '#fff' }}
+                <Button color="dark" w={'100%'} size="lg">
+                  <Link
+                    style={{ textDecoration: 'none', color: '#fff' }}
                     href={{
                       pathname: `/newsale/payment-confirmation/${contractData.data}/${paymentCurrency}`,
                     }}
@@ -198,10 +247,9 @@ export default function PaymentScan() {
                 </Button>
               </Paper>
             </>
-          }
-        </Flex >
-      </Layout >
+          )}
+        </Flex>
+      </Layout>
     </>
   );
 }
-
